6d1545a90eda2b555f39dd7b01a8dfb8e48f39c4
Daniel Woznicki <daniel.woznicki@gmail.com>
2024-12-05 00:40:33 -0800

    Copy change.

--- routes/about.tsx
+++ routes/about.tsx
@@ -5,7 +5,12 @@ const AboutPage = () => {
       <div className="grid grid-cols-3 gap-x-4">
         <img src="/images/profile.jpg" alt="Artist profile picture" />
         <div className="col-span-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum metus et venenatis placerat. Donec in vestibulum dui. Mauris quis libero ultricies nisi finibus placerat a vitae massa. Integer sapien odio, bibendum non suscipit ut, pretium sed est. Donec tincidunt, leo at rutrum viverra, felis ipsum luctus mauris, sed mattis mauris magna ut arcu. Fusce ornare aliquam nibh et posuere. Donec facilisis, mi ut tempus eleifend, felis diam gravida sem, vitae interdum massa odio at felis. Nam a mollis enim. Donec sit amet tristique ex. Morbi eget dapibus metus, sit amet placerat est. Praesent a facilisis ipsum. Nam tincidunt augue at dui interdum, vel facilisis tortor auctor. Integer porta finibus massa sit amet dignissim.
+          <p>
+            The artist is a major cutie.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vestibulum metus et venenatis placerat. Donec in vestibulum dui. Mauris quis libero ultricies nisi finibus placerat a vitae massa. Integer sapien odio, bibendum non suscipit ut, pretium sed est. Donec tincidunt, leo at rutrum viverra, felis ipsum luctus mauris, sed mattis mauris magna ut arcu. Fusce ornare aliquam nibh et posuere. Donec facilisis, mi ut tempus eleifend, felis diam gravida sem, vitae interdum massa odio at felis. Nam a mollis enim. Donec sit amet tristique ex. Morbi eget dapibus metus, sit amet placerat est. Praesent a facilisis ipsum. Nam tincidunt augue at dui interdum, vel facilisis tortor auctor. Integer porta finibus massa sit amet dignissim.
+          </p>
         </div>
       </div>
     </div>
